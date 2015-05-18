f9484c2367481fdbffcc4811b9b5b171291b1291
Ben Petering <ben@petering.net>
2015-05-18 19:58:27 +0930

    Add IRC server code and update slides

---
+++ irc_server01.py
@@ -0,0 +1,40 @@
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+
+import logging
+
+PORT = 6667
+
+
+class PseudoIRC(LineReceiver):
+
+    def __init__(self):
+        pass
+
+    def connectionMade(self):
+        logging.info("Got connection")
+
+    def connectionLost(self, reason):
+        logging.info("Lost connection")
+
+    def lineReceived(self, line):
+        logging.info("Received line: %s" % line)
+
+
+class PseudoIRCFactory(Factory):
+
+    def __init__(self):
+        self.channels = {}
+        self.nicks = {}
+
+    def buildProtocol(self, addr):
+        return PseudoIRC()
+
+
+logging.basicConfig(level=logging.INFO)
+
+reactor.listenTCP(PORT, PseudoIRCFactory())
+logging.info("Server listening on port %d" % PORT)
+
+reactor.run()
